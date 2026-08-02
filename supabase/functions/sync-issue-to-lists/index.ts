ed71dda7231f30b1d7a9deed6718bc884f75cf98
Chad <lumpsrgood@gmail.com>
2026-08-01 19:05:18 -0500

    Restore Team list alert target

--- supabase/functions/sync-issue-to-lists/index.ts
+++ supabase/functions/sync-issue-to-lists/index.ts
@@ -72,7 +72,7 @@ function alertCaseId(rawId: string) {
 function guestCasesUrlFor(rawId: string) {
   const filterValue = encodeURIComponent(alertCaseId(rawId));
   const listUrl = Deno.env.get("POWER_AUTOMATE_GUEST_CASES_URL") ||
-    "https://opportunityrestaurantgroup-my.sharepoint.com/personal/gchadrick_opportunityrestaurantgroup_com/Lists/Guest%20Cases/AllItems.aspx";
+    "https://opportunityrestaurantgroup.sharepoint.com/sites/GuestRelations/Lists/Guest%20Cases/AllItems.aspx";
   return `${listUrl}?FilterField1=Title&FilterValue1=${filterValue}`;
 }
 
